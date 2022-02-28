3773134195e533a5a69731391ae0506ffbe33bb8
bgoonz <bryan.guner@gmail.com>
2022-02-28 00:58:09 +0000

    -update

---
+++ src/utils/attribute.js
@@ -0,0 +1,11 @@
+export default function attribute(name, value, condition) {
+    if (typeof condition === 'undefined') {
+        condition = true;
+    }
+
+    return condition
+        ? {
+              [name]: value
+          }
+        : null;
+}
